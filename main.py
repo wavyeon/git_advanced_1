f352616911de9889ccda5bde2c66a1600bc03019
wavyeon <jjjjun0906@naver.com>
2024-04-18 19:16:01 +0900

    sum_of_squares_of_even commit

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
   """    
   
   # TODO: Implement sum_of_squares_of_even
-  pass
+  sum_of_squares = 0
+    
+  for num in even_int_list:
+    sum_of_squares += num ** 2
+  return sum_of_squares
 
 #Main function
 def main():    
